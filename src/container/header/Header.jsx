b027dd7d069e26639b20de78250ea1484b18c149
ccfine <469781947@qq.com>
2018-09-13 23:46:57 +0800

    开发Header组件

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react"
-import { Link, withRouter } from "react-router-dom"
 import logo from "assets/logo.png"
 import styles from "./header.css"
-
-@withRouter
 
 export default class Header extends Component {
   render () {
     return (
       <div className={ styles.container }>
-        
+        <div className={ styles["content-container"] }>
+          <img src={ logo } alt="简书logo" className={ styles.logo } />
+          <span className={ styles.home }>首页</span>
+          <span className={ styles.home }>下载App</span>
+        </div>
+        <div></div>
       </div>
     )
   }
--- src/page/error/Error.jsx
+++ src/page/error/Error.jsx
@@ -4,9 +4,9 @@ import styles from "./error.css"
 
 export const Error = () => {
   return (
-    <div className="container flex-center">
+    <div className="container">
       <Title title="出错啦！！！" />
-      <div className="flex-center flex-column">
+      <div>
         <h1 className={ styles.error }>404</h1>
         <h1 className={ styles.error }>NOT FOUND</h1>
       </div>
